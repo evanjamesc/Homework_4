fbdae3cffceb0d20bb832ad2d458f49dcde71d19
Evan Christianson <evanjameschristianson@gmail.com>
2020-10-24 19:10:44 -0500

    Updated button function

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function startQuiz() {
-    alert("Button clicked");
-}
+ $(document).ready(function() { // this waits until the document is fully loaded
+   
+    var newButton = $("<button>");
+    var newDiv = $("<div>");
+   
+    // $("#testDiv").on("click", function () {
 
-$("#startQuiz").on("click", function () {
-    alert("Button clicked");
+    //     alert("Clicked");
+
+    // });
+
+    $("#startQuiz").on("click", function () {
+      //  alert("Cliiiiicked");
+        $("#startQuiz").text("Hello");
+
+        $(".card-body").append(newDiv);
+        $(".card-body").append(newButton).attr('id', 'button1');
+
+        $(".card-body").append(newDiv);
+        $(".card-body").append(newButton).attr('id', 'button1');
+    });
 });
